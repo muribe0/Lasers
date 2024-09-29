dc36e4816f75984397ddffedf07419725a4e3a9d
muribe <muribe@fi.uba.ar>
2024-09-29 05:11:00 -0300

    Direccion es una subclase (hijo) de Coordenada. Se usa para mantener esa paralabra para toda clase que requiera una direccion. Ademas, se encarga de crear una Direccion a base de una String del tipo NE o SE,...

--- src/main/java/modelo/Direccion.java
+++ src/main/java/modelo/Direccion.java
@@ -1,4 +1,22 @@
 package modelo;
 
-public class Direccion {
+public class Direccion extends Coordenada {
+    public Direccion(int x, int y) {
+        super(x, y);
+    }
+
+    public Direccion(String direccion) {
+        super(
+                switch (direccion) {
+                    case "NE", "SE" -> 1;
+                    case "NW", "SW" -> -1;
+                    default -> throw new IllegalArgumentException("Dirección inválida: " + direccion);
+                },
+                switch (direccion) {
+                    case "NE", "NW" -> -1;
+                    case "SE", "SW" -> 1;
+                    default -> throw new IllegalArgumentException("Dirección inválida: " + direccion);
+                }
+        );
+    }
 }
