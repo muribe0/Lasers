f27b7e3407df1da4e6ef1c012c427efcfe263446
muribe <muribe@fi.uba.ar>
2024-10-05 04:15:36 -0300

    interaccion con laser actualizada

--- src/main/java/modelo/BloqueOpacoMovil.java
+++ src/main/java/modelo/BloqueOpacoMovil.java
@@ -2,6 +2,10 @@ package modelo;
 
 public class BloqueOpacoMovil extends Bloque {
 
+    public BloqueOpacoMovil(Integer dimension) {
+        super(dimension);
+    }
+
     @Override
     public boolean esMovible() {
         return true;
@@ -13,21 +17,10 @@ public class BloqueOpacoMovil extends Bloque {
     }
 
     @Override
-    public void interactuarConLaser(Emisor emisor) {
-        emisor.detener();
+    public void interactuarConLaser(Laser laser) {
+        laser.detener();
     }
 
-    // Juego llame a Emisor.emitir() cada vez que se mueva un bloque
-    // Grilla tiene que haber un metodo que intercambie dos bloques
-
-    // Si hizo clic en un bloque y fue movible, me guardo las Coordenadas
-    // Si volves a hacer clic tengo las Coordenadas, ejecutas el metodo de Grilla.moverBloque()
-
-    // Ver si la coordenada que le pasas es de un bloque vacio, la coordenada origen
-    // moverBloque() {
-    // se fija si la coordenada origen es Movil, si el destino es vacio
-    //
-    //}
 
     @Override
     public String toString() {
