4d6eaa2bf27986476f75092b68ebc3b886b05683
muribe <muribe@fi.uba.ar>
2024-10-12 20:16:56 -0300

    agrego documentacion y ahora los rectangulos seleccionables se inicializan con un listener para cambiar su color al ser seleccionados

--- src/main/java/vista/VistaGrilla.java
+++ src/main/java/vista/VistaGrilla.java
@@ -19,7 +19,9 @@ public class VistaGrilla extends GridPane {
         inicializarBloques();
     }
 
-    // Inicializa la vista visual de los bloques basados en el modelo.
+    /**
+     * Inicializa los bloques y los coloca en en GridPane.
+     */
     public void inicializarBloques() {
         Integer dimensionBloque = grilla.getDimensionBloque();
         for (int x = 0; x < grilla.getAncho(); x++) {
@@ -27,12 +29,18 @@ public class VistaGrilla extends GridPane {
                 Coordenada coordenada = new Coordenada(x * dimensionBloque, y * dimensionBloque);
                 Bloque bloque = grilla.getBloque(coordenada);
                 Rectangle bloqueVisual = crearRectanguloParaBloque(bloque, dimensionBloque * tamanioBloque);
-                this.add(bloqueVisual, x , y );
+                this.add(bloqueVisual, x, y);
             }
         }
     }
 
-    // Método auxiliar para crear un rectángulo basado en el tipo de bloque.
+    /**
+     * Inicializa un rectangulo de acuerdo al tipo de bloque con listeners para los bloques seleccionables.
+     *
+     * @param bloque:          bloque a colocar en la grilla
+     * @param dimensionBloque: dimension del bloque en relacion a la grilla
+     * @return
+     */
     private Rectangle crearRectanguloParaBloque(Bloque bloque, int dimensionBloque) {
         Rectangle rect = new Rectangle(dimensionBloque, dimensionBloque);
         if (bloque == null) {
@@ -41,22 +49,36 @@ public class VistaGrilla extends GridPane {
             rect.setFill(Color.GRAY);
         } else if (bloque instanceof modelo.BloqueOpacoMovil) {
             rect.setFill(Color.DARKGRAY);
+            rect.setOnMouseClicked(e -> {
+                rect.setFill(Color.DARKSLATEGRAY);
+            });
         } else if (bloque instanceof modelo.BloqueEspejo) {
-            rect.setFill(Color.PURPLE);
+            rect.setFill(Color.MEDIUMPURPLE);
+            rect.setOnMouseClicked(e -> {
+                rect.setFill(Color.PURPLE);
+            });
         } else if (bloque instanceof modelo.BloqueVidrio) {
             rect.setFill(Color.LIGHTBLUE);
+            rect.setOnMouseClicked(e -> {
+                rect.setFill(Color.BLUEVIOLET);
+            });
         } else if (bloque instanceof modelo.BloqueCristal) {
             rect.setFill(Color.BLUE);
+            rect.setOnMouseClicked(e -> {
+                rect.setFill(Color.DARKBLUE);
+            });
         } else if (bloque instanceof modelo.BloqueVacio) {
             rect.setFill(Color.BEIGE);
         } else if (bloque instanceof modelo.BloqueSinPiso) {
             rect.setFill(Color.BLACK);
         }
-        rect.setStroke(Color.DARKORANGE);
+        rect.setStroke(Color.BLACK);
         return rect;
     }
 
-    // Método para actualizar la vista de la grilla cuando se modifique el modelo.
+    /**
+     * Actualiza la vista para reflejar los cambios del nivel. Para ello, limpia la vista y vuelve a inicializar los bloques.
+     */
     public void actualizarVista() {
         this.getChildren().clear();
         inicializarBloques();
