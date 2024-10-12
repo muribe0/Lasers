9b3024b6ab0b915da09d13925915e724a8521d19
muribe <muribe@fi.uba.ar>
2024-10-12 02:07:34 -0300

    agrego documentacion de interactuarConLaser()

--- src/main/java/modelo/Bloque.java
+++ src/main/java/modelo/Bloque.java
@@ -7,11 +7,17 @@ public abstract class Bloque { // Considerar implementar una interfaz para los b
         this.dimension = dimension;
     }
 
-    // Mét.odo abstracto que cada subclase debe implementar.
+    /**
+     * Interacctua con el Laser utilizando los metodos de Laser de acuerdo a como incida el mismo al bloque y el tipo de bloque que implemente Bloque
+     *
+     * @param laser: laser incidente al bloque
+*    * @post: el laser fue reflejado, refractado, movido, etc. segun el tipo de bloque
+     */
     public abstract void interactuarConLaser(Laser laser);
 
     /**
      * Devuelve True si una coordenada se encuentra en el borde horizontal de un bloque
+     *
      * @param coordenada: coordenada a verificar
      * @return True si la coordenada se encuentra en el borde horizontal de un bloque
      */
@@ -21,6 +27,7 @@ public abstract class Bloque { // Considerar implementar una interfaz para los b
 
     /**
      * Devuelve True si una coordenada se encuentra en el borde vertical de un bloque
+     *
      * @param coordenada: coordenada a verificar
      * @return True si la coordenada se encuentra en el borde vertical de un bloque
      */
