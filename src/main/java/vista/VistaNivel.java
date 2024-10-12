391e3f7dadb54c5a64b6e79007d0a198cdf2155e
muribe <muribe@fi.uba.ar>
2024-10-12 20:20:39 -0300

    arreglo BUGS: 1. Ahora cada click es transmitido a la grilla y al bloque seleccionado especificamente. Ya que las capas superiores a la grilla son MouseTransparent. 2. Ahora el bloque se marca como seleccionado si es que es uno seleccionable (movible) 3. Ahora se actualizan los objetivos y se valida la solucion cada vez que hay un cambio en el nivel 4. Se logre movel el bloque o no, se actualiza el nivel y sus vistas. 5. Pequenia migracion del handler a una clase MoverBloque que implementa EventHandler<MouseEvent>

--- src/main/java/vista/VistaNivel.java
+++ src/main/java/vista/VistaNivel.java
@@ -1,22 +1,17 @@
 package vista;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import modelo.Coordenada;
 import modelo.Nivel;
-import modelo.Objetivo;
 
 public class VistaNivel extends StackPane {
     private Nivel nivel;
-    private Integer alto;
-    private Integer ancho;
-
 
     private VistaGrilla panelGrilla;
     private VistaLasers panelLaser;
+    private VistaObjetivos panelObjetivos;
 
     private final Integer tamanioBloque;
 
@@ -25,73 +20,69 @@ public class VistaNivel extends StackPane {
     Integer origenY;
 
 
+    /**
+     * Crea una instancia de la Vista de un Nivel alineado en la esquina superior izquierda respecto a su contenedor.
+     * Inicializa un GridPanel y StackPane(s) para representar la grilla, los lasers y objetivos del nivel.
+     * Agrega un listener para mover bloques y actualizar el estado del juego luego de moverlos.
+     * @param nivel: Nivel de la vista
+     * @param tamanioBloque: Tamanio de cada bloque del Nivel en pixeles.
+     */
     public VistaNivel(Nivel nivel, Integer tamanioBloque) {
         this.setAlignment(Pos.TOP_LEFT);
 
         this.nivel = nivel;
-        this.alto = nivel.getGrilla().getAlto();
-        this.ancho = nivel.getGrilla().getAncho();
 
         this.bloqueSeleccionado = false;
         this.tamanioBloque = tamanioBloque;
 
         this.panelGrilla = new VistaGrilla(nivel.getGrilla(), tamanioBloque);
-        this.panelLaser = new VistaLasers(nivel, tamanioBloque);
+        this.panelLaser = new VistaLasers(nivel.getEmisores(), tamanioBloque);
+        this.panelObjetivos = new VistaObjetivos(nivel.getObjetivos(), tamanioBloque);
+        panelLaser.setMouseTransparent(true);
+        panelObjetivos.setMouseTransparent(true);
 
-        this.getChildren().addAll(panelGrilla, panelLaser);
+        this.getChildren().addAll(panelGrilla, panelLaser, panelObjetivos);
         // panelLaser esta encima
-        agregarEventosDeBloque();
-        dibujarObjetivos();
+        this.setOnMouseClicked(event -> new MoverBloque().handle(event));
+
     }
 
-    private void agregarEventosDeBloque() {
-
-        // Evento cuando se hace clic sobre un bloque o celda de la grilla
-        this.setOnMouseClicked(event -> {
+    /**
+     * Listener que se encarga de mover el bloque y actualizar el nivel, estado de juego y vistas luego de moverlo.
+     *
+     */
+    public class MoverBloque implements EventHandler<MouseEvent> {
+        @Override
+        public void handle(MouseEvent evento) {
             if (nivel.estaCompletado()) {
                 return;
             }
             if (!bloqueSeleccionado) {
                 // Primer clic: Seleccionar el bloque
-                origenX = (int) event.getX();
-                origenY = (int) event.getY();
-                bloqueSeleccionado = true;
+                origenX = (int) evento.getX();
+                origenY = (int) evento.getY();
+                if (nivel.bloqueEsMovible(origenX / tamanioBloque, origenY / tamanioBloque)) {
+                    bloqueSeleccionado = true;
+                }
             } else {
                 // Segundo clic: Mover el bloque seleccionado a la nueva posición
-                int destinoX = (int) event.getX();
-                int destinoY = (int) event.getY();
+                int destinoX = (int) evento.getX();
+                int destinoY = (int) evento.getY();
 
                 // Llamar a la lógica del juego para mover el bloque
-                nivel.moverBloque(origenX / tamanioBloque, origenY / tamanioBloque, destinoX / tamanioBloque, destinoY / tamanioBloque);
+                nivel.moverBloque(origenX / tamanioBloque, origenY / tamanioBloque,
+                        destinoX / tamanioBloque, destinoY / tamanioBloque);
                 panelGrilla.actualizarVista();
                 panelLaser.inicializarEmisores();
 
                 // Reiniciar el estado de selección
                 bloqueSeleccionado = false;
 
+                nivel.actualizarObjetivos();
                 nivel.validarSolucion();
             }
-        });
-    }
-
-    // Método para dibujar los objetivos
-    private void dibujarObjetivos() {
-        for (Objetivo objetivo : nivel.getObjetivos()) {
-            // Obtener la posición del objetivo en la grilla
-            Coordenada posicion = objetivo.getPosicion();
-            int columna = posicion.getX();
-            int fila = posicion.getY();
-
-            // Crear un rectángulo que represente el objetivo
-            Rectangle rect = new Rectangle((double) tamanioBloque / 8, (double) tamanioBloque / 8);
-            rect.setFill(Color.RED); // Color del objetivo
-
-            rect.setTranslateX(columna * tamanioBloque);
-            rect.setTranslateY(fila * tamanioBloque);
-
-            // Posicionar el rectángulo en la grilla
-            this.getChildren().add(rect); // Añade el objetivo en la posición de la grilla
         }
-    }
 
+    }
 }
+
